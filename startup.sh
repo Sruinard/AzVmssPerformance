1157dbcead62fd922fe1dd975afc76a8dff3077b
Stef Ruinard <stefruinard@hotmail.com>
2021-08-25 13:44:27 +0200

    startupscript

--- startup.sh
+++ startup.sh
@@ -9,6 +9,16 @@ storageAccountKey=$(az storage account keys list \
     --account-name $storageAccountName \
     --query "[0].value" | tr -d '"')
 
+sudo mkdir /mnt/vmssfileshare
+if [ ! -d "/etc/smbcredentials" ]; then
+sudo mkdir /etc/smbcredentials
+fi
+if [ ! -f "/etc/smbcredentials/vmsssaklm.cred" ]; then
+	echo 'username=vmsssaklm' >> /etc/smbcredentials/vmsssaklm.cred;
+ 	echo "password=$storageAccountKey" >> /etc/smbcredentials/vmsssaklm.cred; 
+fi
+sudo chmod 600 /etc/smbcredentials/vmsssaklm.cred
 
-sudo mount -t cifs //vmsssaklm.file.core.windows.net/vmssfileshare /mnt/vmssfileshare -o vers=3.0,credentials='password=${storageAccountKey}'
+sudo bash -c 'echo "//vmsssaklm.file.core.windows.net/vmssfileshare /mnt/vmssfileshare cifs nofail,vers=3.0,credentials=/etc/smbcredentials/vmsssaklm.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
+sudo mount -t cifs //vmsssaklm.file.core.windows.net/vmssfileshare /mnt/vmssfileshare -o vers=3.0,credentials=/etc/smbcredentials/vmsssaklm.cred,dir_mode=0777,file_mode=0777,serverino
 
