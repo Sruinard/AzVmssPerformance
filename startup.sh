73e9f367654f41a0ae3df9eb419e5368c9cb2516
Stef Ruinard <stefruinard@hotmail.com>
2021-08-25 18:34:51 +0200

    startupfile

--- startup.sh
+++ startup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # install azurecli
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo apt-get update
+sudo apt-get install azure-cli
 
 # login with managed identity
 az login --identity
@@ -14,7 +26,9 @@ storageAccountKey=$(az storage account keys list \
     --query "[0].value" | tr -d '"')
 
 # make directories and write credentials to file
+if [ ! -d "/mnt/vmssfileshare" ]; then
 sudo mkdir /mnt/vmssfileshare
+fi
 if [ ! -d "/etc/smbcredentials" ]; then
 sudo mkdir /etc/smbcredentials
 fi
