506363c679089c5e4b51c5ad813990a414379d93
Stef Ruinard <stefruinard@hotmail.com>
2021-08-31 14:47:31 +0200

    update logging

--- run_logging_extension.sh
+++ run_logging_extension.sh
@@ -7,4 +7,4 @@ az vmss extension set \
     --name CustomScript \
     --vmss-name vmss-instance \
     --resource-group vmss-performance \
-    --settings "{\"fileUris\":[\"https://raw.githubusercontent.com/Sruinard/AzVmssPerformance/master/echo_env_var.py\", \"https://raw.githubusercontent.com/Sruinard/AzVmssPerformance/master/run_logging_extension.sh\"],\"commandToExecute\":\"./run_logging_extension.sh $1 $2\"}"
+    --settings "{\"fileUris\":[\"https://raw.githubusercontent.com/Sruinard/AzVmssPerformance/master/echo_env_var.py\", \"https://raw.githubusercontent.com/Sruinard/AzVmssPerformance/master/logging.sh\"],\"commandToExecute\":\"./logging.sh $1 $2\"}"
